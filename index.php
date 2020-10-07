ea8cd7a6b7c5ff8353e66818af252170da46b33a
unisoft2020 <support@unisoft.am>
2020-10-07 20:45:14 +0400

    firs commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+require 'config.php';
+require 'common/php/header.php';
+require 'src/php/templates.php';
+?>
+
+<div class="kr_content_wrapper" id="<?= ID() ?>" data-version="<?= VERSION() ?>">
+  <div class="container">
+    <h1><?= word('TXT_TITLE')?></h1>
+    <h2><?= word('TXT_TEXT_1')?></h2>
+  </div>
+</div>
+
+<?php require 'common/php/footer.php';
